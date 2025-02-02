2b0326d15cf3996a692c6ed20fd403d61f14764b
Alex <aksenov.houme@gmail.com>
2025-02-02 23:01:35 +0300

    lifespan_and_requirements

--- fastapi-application/main.py
+++ fastapi-application/main.py
@@ -1,21 +1,26 @@
 import uvicorn
+from contextlib import asynccontextmanager
+from core.db_helper import db_helper
 from fastapi import FastAPI
 
 from api import router as api_router
 from core.config import settings
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    yield
+    # shutdown
+    print("exit!!!!!!!!!!")
+    await db_helper.dispose()
+
+app = FastAPI(
+    lifespan=lifespan,
+    )
 app.include_router(
     api_router,
     prefix=settings.api.prefix,
 )
-
-
-@app.get("/")
-def home():
-    return {"message": "Hello world!"}
-
-
 
 
 if __name__ == "__main__":
